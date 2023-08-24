a2db21fa66a820ff0e510d11c8c7786be371d399
longfar-ncy <77976092+longfar-ncy@users.noreply.github.com>
2023-08-24 20:16:16 +0800

    Update start_douyin.sh
    fix test config path

--- tests/start_douyin.sh
+++ tests/start_douyin.sh
@@ -1,7 +1,7 @@
 go mod tidy && go run test_init_data.go
 cd .. && go mod tidy && go build .
 echo '编译完成'
-nohup ./douyin -c ../tests/test_config.yml&
+nohup ./douyin -c tests/test_config.yml&
 while ! nc -z localhost 30000; do
     sleep 1
 done
